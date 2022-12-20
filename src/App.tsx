f51ac1760b95d5fe12ae33f3fff7e6491391f691
jorgekean <jkn379@gmail.com>
2022-12-20 19:49:16 +0800

    change image src

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
             <nav className="navbar navbar-expand-lg navbar-dark bg-success fixed-top" id="sideNav">
                 <a className="navbar-brand js-scroll-trigger" href="#page-top">
                     <span className="d-block d-lg-none">{firstName} {lastName}</span>
-                    <span className="d-none d-lg-block"><img className="img-fluid img-profile rounded-circle mx-auto mb-2" src="src/assets/img/profile-jkn.jpg" alt="..." /></span>
+                    <span className="d-none d-lg-block"><img className="img-fluid img-profile rounded-circle mx-auto mb-2" src="https://github.com/jorgekean/jkn-profile/blob/main/src/assets/img/profile-jkn.jpg?raw=true" alt="..." /></span>
                 </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
                 <div className="collapse navbar-collapse" id="navbarResponsive">
